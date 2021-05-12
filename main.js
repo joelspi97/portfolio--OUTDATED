5c65cf634301bf453e4158b0a74c2a034d1ab176
Joel Spinelli <joelspi97@gmail.com>
2021-05-12 16:23:31 -0300

    added animation for hamburger menu

---
+++ main.js
@@ -0,0 +1,15 @@
+// HAMBURGER MENU ANIMATION
+
+const menuBtn = document.querySelector(".menu-btn");
+let menuOpen = false;
+
+menuBtn.addEventListener("click", () => {
+    if(!menuOpen) {
+        menuBtn.classList.add("open");
+        menuOpen = true;
+    }
+    else {
+        menuBtn.classList.remove("open");
+        menuOpen = false;
+    }
+});
