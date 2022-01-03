a3eefd3d3c0b32a6b7f5f913f3d1f68e92439bfd
joel <joelspi97@gmail.com>
2022-01-03 15:46:53 -0300

    added some accessibility changes

--- main.js
+++ main.js
@@ -1,15 +1,44 @@
 // HAMBURGER MENU ANIMATION
 
-const menuBtn = document.querySelector(".menu-btn");
+const menuBtn = document.querySelector('.menu-btn');
+const checkInput = document.getElementById('toggle');
+const navLinks = Array.from(document.querySelectorAll('.nav-link'));
 let menuOpen = false;
 
-menuBtn.addEventListener("click", () => {
-    if(!menuOpen) {
-        menuBtn.classList.add("open");
+function toggleMenu() {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuBtn.setAttribute('aria-label', 'Close menu');
         menuOpen = true;
-    }
-    else {
-        menuBtn.classList.remove("open");
+    } else {
+        menuBtn.classList.remove('open');
+        menuBtn.setAttribute('aria-label', 'Open menu');
         menuOpen = false;
     }
+}
+
+function closeMenu() {
+    if (menuOpen) {
+        menuBtn.classList.remove('open');
+        menuBtn.setAttribute('aria-label', 'Open menu');
+        menuOpen = false;
+        checkInput.checked = false;
+    }
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+menuBtn.addEventListener('keyup', e => {
+    if (e.code === 'Enter') { 
+        toggleMenu();
+
+        if (menuOpen) {
+            checkInput.checked = true;
+        } else {
+            checkInput.checked = false;
+        }
+    };
 });
+
+navLinks.forEach(l => {
+    l.addEventListener('click', closeMenu);
+})
